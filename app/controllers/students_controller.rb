a7b467f5f180eb58795e0b896211fe7d490adaf8
Amelia Manders <amelia.manders@suny.edu>
2015-02-21 17:23:33 -0500

    sort on lastname

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,7 @@ class StudentsController < ApplicationController
     if params[:classroom]
       @students = Student.where(:classroom => params[:classroom])
     else
-      @students = Student.all
+      @students = Student.all.order(:last_name)
     end
     #respond_with(@students)
     create_email_string(@students)
@@ -41,56 +41,6 @@ class StudentsController < ApplicationController
     @student.destroy
     respond_with(@student)
   end
-  
-# def movers
-#   @students = Student.where(:classroom => "Movers")
-#   create_email_string(@students)
-# end
-# 
-# def wigglers
-#   @students = Student.where(:classroom => "Movers")
-#    create_email_string(@students)
-# end
-# 
-# def shakers
-#   @students = Student.where(:classroom => "Shakers")
-#    create_email_string(@students)
-# end
-# 
-# def creators
-#   @students = Student.where(:classroom => "Creators")
-#    create_email_string(@students)
-# end
-# 
-# def experimenters
-#   @students = Student.where(:classroom => "Experimenters")
-#    create_email_string(@students)
-# end
-# 
-# def investigators
-#   @students = Student.where(:classroom => "Investigators")
-#    create_email_string(@students)
-# end
-# 
-# def dreamers
-#   @students = Student.where(:classroom => "Dreamers")
-#    create_email_string(@students)
-# end
-# 
-# def inventors
-#    @students = Student.where(:classroom => "Inventors")
-#     create_email_string(@students)
-# end
-#  
-# def explorers
-#   @students = Student.where(:classroom => "Explorers")
-#    create_email_string(@students)
-# end
-# 
-# def explorers
-#   @students = Student.where(:classroom => "Explorers")
-#    create_email_string(@students)
-# end
   
   def create_email_string(string)
      @string = string.collect(&:email1).join(';')
